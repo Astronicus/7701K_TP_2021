efd329fbae41dcc36122067004de949fd416cd0e
Astronicus <astronicusflame@gmail.com>
2021-11-16 00:37:21 -0500

    clampMotor changed to move_relative for no buttons

--- .vscode/cquery_cached_index/c@@users@joey@documents@github@7701k_tp_2021/src@control@clamp.cpp
+++ .vscode/cquery_cached_index/c@@users@joey@documents@github@7701k_tp_2021/src@control@clamp.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int clampVoltage = 100;
+int clampVelocity = 100;
 
 void clampControl(){
   clampMotor.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -9,15 +9,15 @@ void clampControl(){
   bool openClamp = master.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
 
   if(closeClamp && openClamp){
-    clampMotor.move(0);
+    clampMotor.move_velocity(0);
   }
   else if (closeClamp){
-    clampMotor.move(clampVoltage);
+    clampMotor.move_velocity(clampVelocity);
   }
   else if (openClamp){
-    clampMotor.move(-clampVoltage);
+    clampMotor.move_velocity(-clampVelocity);
   }
   else{
-    clampMotor.move(0);
+    clampMotor.move_relative(0, 0);
   }
 }
